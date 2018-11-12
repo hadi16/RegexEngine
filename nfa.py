c900010551baf59c8b5e0d6f05815996cd8dcc43
chelle-plaisted <plaisted20@up.edu>
2018-11-11 18:50:29 -0800

    expanded nfa.py to start adding support for concatenation

--- nfa.py
+++ nfa.py
@@ -1,8 +1,75 @@
-from node import Node
 from typing import List
+# constant to define 'epsilon'
+EPSILON = 0
 
+# Class to define a state of an NFA, identifiable by an id.
+class State:
+    def __init__(self, id):
+        self.id = id
 
-class Nfa:
-    def __init__(self, initial_node: Node, accepting_nodes: List[Node]):
-        self.initial_node = initial_node
-        self.accepting_nodes = accepting_nodes
+# class to define the structure of an NFA with helper functions to help build it.
+# NOTE: This class will support nondeterministic EPSILON transitions, but will NOT
+# support having multiple transitions on the same char from a given state.
+class NFA:
+    def __init__(self, initial_state: Node = None, accepting_states: List[Node] = None):
+        self.initial_state = None
+        self.accepting_states = []
+        self.alphabet = []
+        self.states = []
+        self.transition_function = {} # structure: {(state, char) => state}
+
+    ##
+    # initialize
+    #
+    # Description: initialize the NFA by adding an initial state and alphabet.
+    #
+    # Parameters:
+    #   Alphabet: the input alphabet for this NFA
+    ##
+    def initialize(self, alphabet):
+        self.alphabet = alphabet
+        initial_state = State(len(states))
+        self.states.append(initial_state)
+        self.initial_state = initial_state
+
+    ##
+    # addNormalChar
+    #
+    # Description: Add necessary states and transitions to connect one state to
+    # a new one on some character. Invariants: state is in self.states(), char is in
+    # self.alphabet. Assume final state should be an accepting state unless told otherwise.
+    #
+    # Parameters:
+    #   old_state : the state to transition from.
+    #   transition_char: char to transition on
+    #
+    # Return : the state representing the final state of this sequence.
+    ##
+    def addNormalChar(self, old_state, transition_char, assume_accept = True):
+        # build a new state to transition to
+        new_state = State(len(states))
+        # add state to states
+        self.states.append(new_state)
+        # add state to accepting states
+        if assume_accept:
+            self.accepting_states = new_state # TODO currently overwrites old
+            # accept states. Will need to change when functions other than concatenation are supported.
+        # add transiton from old_state to new_state
+        self.transition_function((old_state, transiton_char)) = new_state
+
+        return new_state
+
+    ##
+    # addEpsilonConnector
+    #
+    # Description: Add necessary states and transitions to connect one state to
+    # a new one on epsilon. Invariants: state is in self.states(). New state will
+    # not be accepting.
+    #
+    # Parameters:
+    #   old_state : the state to transition from.
+    #
+    # Return : the state representing the final state of this sequence.
+    ##
+    def addEpsilonConnector(self, old_state):
+        return self.addNormalChar(old_state, EPSILON, False)
